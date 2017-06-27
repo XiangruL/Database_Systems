a886c0497504a87e0834871e911224811d94bccb
Fengyu Wu <fengyuwu@buffalo.edu>
2017-06-26 20:17:34 -0400

    Finished OrderBy
    Evel.java  contains main function and test cases.
    Orderby.java is the implementation: Return you a string of ArrayList.
    
    Testing function: output=cro.orderBy("publication.csv", "pubTypeID", 0,null,"Int");
    
    
    publication.csv is hard address
    pubTypeID is table column
    0 is table column position to be sorted
    null is sort ascending and  "DESC" or any other string will sort it using descend.
    Int is the column attribute.

---
+++ Orderby.java
@@ -0,0 +1,252 @@
+package ParseSqlQuery;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Locale;
+
+public class Orderby {
+	
+	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<String> orderBy(String tableName1, String colName1,int colPos, String condtion,String colType){
+		ArrayList<String> WholeTable = new ArrayList<String>();
+		ArrayList<String> OutputTable = new ArrayList<String>();
+		ArrayList FinalTable = new ArrayList();
+
+		
+		File table1 = new File("C:/Users/Dan/workspace/562/src/publication.csv");
+
+		 //System.out.println(tableName1+".csv");
+
+		String line1 = "";
+		String line2 = "";
+		FileInputStream fr1;
+		try {
+			fr1 = new FileInputStream(table1);
+
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(fr1));
+			ArrayList<String> SortedAttribute = new ArrayList<String>();
+			while((line1 = br1.readLine())!=null){
+			line1  = line1.replaceAll("﻿", "");
+				WholeTable.add(line1);
+				if (colPos == 0){
+					int start = 0;
+					int end = line1.indexOf("|", line1.indexOf("|") + colPos-1);
+					String temp = line1.substring(start,end);
+					SortedAttribute.add(temp);
+				}
+				
+				
+				else{
+//					String temp = line1+"|"+line2;
+					
+				int start =	ordinalIndexOf(line1,"|",colPos)+1;
+			
+				int end = 	ordinalIndexOf(line1,"|",colPos+1);
+				
+				if (end ==-1 ){
+					end = line1.length();
+				}
+//				System.out.println(start);
+//				System.out.println(end);
+				
+				
+				
+				String temp = line1.substring(start,end);
+				//System.out.println(temp);
+				SortedAttribute.add(temp);
+				}
+
+				
+			}
+			//case 1 Int
+			if (colType == "Int"){
+			ArrayList<Integer> arrayOfInts = new ArrayList<Integer>();
+			for (String str : SortedAttribute) {
+			   arrayOfInts.add(Integer.parseInt((String)str));
+			}
+
+			if (condtion==null){
+				Collections.sort(arrayOfInts);
+			}
+			else {
+				Collections.sort(arrayOfInts);
+				Collections.reverse(arrayOfInts);
+//				System.out.println(arrayOfInts);
+
+			
+			}	
+		      for (Integer i:arrayOfInts){
+		    	  FinalTable.add(i);
+		      }
+
+			}
+			
+			//case 2 Double
+			
+			if (colType == "Double"){
+				ArrayList<Double> arrayOfDouble = new ArrayList<Double>();
+				for (String str : SortedAttribute) {
+					arrayOfDouble.add(Double.parseDouble((String)str));
+				}
+
+				if (condtion==null){
+					Collections.sort(arrayOfDouble);
+				}
+				else {
+					Collections.sort(arrayOfDouble);
+					Collections.reverse(arrayOfDouble);
+				
+				}	
+			      for (Double i:arrayOfDouble){
+			    	  FinalTable.add(i);
+			      }
+
+				}
+
+			//case 3 String
+			else if (colType == "String"){
+
+				if (condtion==null){
+					Collections.sort( SortedAttribute);
+					
+				}
+				else {
+					Collections.sort(SortedAttribute);
+					Collections.reverse( SortedAttribute);
+				
+				}
+			      for (String i:SortedAttribute){
+			    	 // System.out.println(i);
+			    	  FinalTable.add(i);
+			      }
+				
+				
+				
+				}
+			//case 4 Date
+				else if (colType == "Date"){
+					ArrayList<Date> arrayOfDate = new ArrayList<Date>();
+
+					
+				
+
+					for (String str : SortedAttribute) {
+
+									
+						
+						arrayOfDate.add(java.sql.Date.valueOf(str));
+					}
+					
+					
+					
+					if (condtion==null){
+						Collections.sort(arrayOfDate);
+//						System.out.println("sort it asc ");
+//						System.out.println(arrayOfDate);
+						
+					}
+					else {
+						Collections.sort(arrayOfDate);
+						Collections.reverse(arrayOfDate);
+//System.out.println("wtf");
+					
+					}
+					
+				      for (Date i:arrayOfDate){
+				    	 // System.out.println(i);
+				    	  FinalTable.add(i);
+				      }
+
+					
+					}
+			
+
+
+			
+
+			
+			
+			
+			
+		    for (int i = 0 ;i <FinalTable.size();i++){
+		    	for (int j = 0; j<WholeTable.size();j++){
+		    		int start;
+		    		int end;
+		    		
+					if (colPos == 0){
+						 start = 0;
+						 end = WholeTable.get(j).indexOf("|", WholeTable.get(j).indexOf("|") + colPos-1);
+		
+					
+					}
+					
+					
+					else{
+
+						
+						start =	ordinalIndexOf(WholeTable.get(j),"|",colPos)+1;
+				
+						end = ordinalIndexOf(WholeTable.get(j),"|",colPos+1);
+					
+					if (end ==-1 ){
+						end = WholeTable.get(j).length();
+					}
+					else {
+						
+					}
+
+					}
+
+					String temp = (String.valueOf(FinalTable.get(i)));
+					String temp2 =WholeTable.get(j).substring(start,end);
+//					System.out.println("temp is "+ temp);
+//					System.out.println("temp2 is " +temp2);
+		    		if (temp.equals(temp2)){
+		    			 //System.out.println("~#dnskfbnjiwebiurgb1iu2gviuv312jkhvrjdnasvhvqweqwejkqw");
+		  
+		    			OutputTable.add(WholeTable.get(j));
+		    			j = WholeTable.size()+1;
+		    		}
+		    		
+		    	}
+		    }
+		      
+		    
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		
+		for (String i:OutputTable){
+		 System.out.println(i);
+		}
+		return OutputTable;
+	}
+	
+	
+	
+	static int ordinalIndexOf(String str, String substr, int n) {
+	    int pos = str.indexOf(substr);
+	    while (--n > 0 && pos != -1){
+	        pos = str.indexOf(substr, pos + 1);
+	    }
+	    return pos;
+	}
+
+}
---
+++ evel.java
@@ -0,0 +1,209 @@
+package ParseSqlQuery;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.*;
+import java.io.File;
+import java.io.FileReader;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.ParseException;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectBody;
+import net.sf.jsqlparser.statement.select.SelectItem;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.*;
+
+public class evel {
+
+
+
+   public static void main(String args[]) throws ParseException, FileNotFoundException {
+	   InputStream inputFile = new FileInputStream(new File("C:/Users/Dan/workspace/562/src/schema.sql"));
+			CCJSqlParser parser = new CCJSqlParser(inputFile);
+			Statement statement;
+			while((statement = parser.Statement()) != null){
+		
+			  if(statement instanceof Select) {
+			  	Select selectStatement = (Select)statement;
+			  	// handle the select
+			  	//system.out.println("This is a Select stat");
+			  	
+			  	SelectBody selectBody = ((Select) statement).getSelectBody();
+			  	//system.out.println( selectBody);
+			  	
+			  	
+			  	//SelectItem selectItem = 
+			  	
+			  	
+			  	
+			  	
+			  	//List<SelectItem> selectItem = ((PlainSelect) statement).getSelectItems();
+			  //	//system.out.println( selectItem);
+		
+				} else if(statement instanceof CreateTable) {
+			  	// and so forth
+					//system.out.println("This is a CreateTable stat");
+			   }
+			}
+			// End-of-file.  Exit!
+			
+			
+			
+			
+			
+			
+			
+			
+      // create array list for six attributes
+      ArrayList <String> projection = new ArrayList();
+      ArrayList <String>from = new ArrayList();
+      ArrayList <String>selection = new ArrayList();
+      ArrayList <String>join = new ArrayList();
+      ArrayList <String>groupby = new ArrayList();
+      ArrayList <String>orderby = new ArrayList();
+      
+      
+      projection.add("R.A");
+      projection.add("R.B");
+      projection.add("R.C");
+      
+      from.add("R");
+      from.add("S");
+      
+      selection.add("R.A");
+      selection.add("R.B");
+      
+      
+//      //system.out.println("Initial size of al: " + al.size());
+//
+//      // add elements to the array list
+//      al.add("C");
+//      al.add("A");
+//      al.add("E");
+//      al.add("B");
+//      al.add("D");
+//      al.add("F");
+//      al.add(1, "A2");
+      ////system.out.println("Size of al after additions: " + projection.size());
+
+      // display the array list
+      String projectionTemp = "PROJECTION: ";
+      
+      for (int i =0 ; i <projection.size();i++) 
+      {
+    	  
+    	  projectionTemp = projectionTemp + projection.get(i);
+    	  if (i != projection.size()-1){
+    		  projectionTemp=projectionTemp+ ", ";
+    	  }
+      }
+      //-----
+      String projectionTemp2 = "FROM: ";
+      
+      for (int i =0 ; i <from.size();i++) 
+      {
+    	  
+    	  projectionTemp2 = projectionTemp2 + from.get(i);
+    	  if (i != from.size()-1){
+    		  projectionTemp2=projectionTemp2+ ", ";
+    	  }
+      }
+      //--------
+      String projectionTemp3 = "SELECTION: ";
+      
+      for (int i =0 ; i <selection.size();i++) 
+      {
+    	  
+    	  projectionTemp3 = projectionTemp3 + selection.get(i);
+    	  if (i != selection.size()-1){
+    		  projectionTemp3=projectionTemp3+ ", ";
+    	  }
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      //Print all six attribute, add null object into Arraylist if nothing was added.
+      if (projection.isEmpty()){
+    	  projection.add(null);
+    	  //system.out.println("PROJECTION: null");
+      }
+      else {
+    	  //system.out.println(projectionTemp);
+      }
+      
+      if (from.isEmpty()){
+    	  from.add(null);
+    	  //system.out.println("FROM: null");
+      }
+      else {
+    	  //system.out.println(projectionTemp2);
+      }
+      if (selection.isEmpty()){
+    	  selection.add(null);
+    	  //system.out.println("SELECTION: null");
+      }
+      else {
+    	  //system.out.println(projectionTemp3);
+      }
+      if (join.isEmpty()){
+    	  join.add(null);
+    	  //system.out.println("JOIN: null");
+      }
+      else {
+    	  //system.out.println(projectionTemp);
+      }
+      if (groupby.isEmpty()){
+    	  groupby.add(null);
+    	  //system.out.println("GROUP-BY: null");
+    	  //ziji xie!!!
+      }
+      else {
+    	  //system.out.println(projectionTemp);
+      }
+      if (orderby.isEmpty()){
+    	  orderby.add(null);
+    	  //system.out.println("ORDER-BY: null");
+      }
+      else {
+    	  //system.out.println(projectionTemp);
+      }
+//      crossProduct cro = new crossProduct();
+//      ArrayList  output = new ArrayList();
+//      output=cro.croPro("author", "authorship");
+//      for (Object i:output){
+//    	  //system.out.println(i);
+//      }
+      
+      Orderby cro = new Orderby();
+      ArrayList  output = new ArrayList();
+      output=cro.orderBy("publication.csv", "pubTypeID", 0,null,"Int");
+      for (Object i:output){
+    	  ////system.out.println(i);
+      }
+      
+      
+      
+      
+      
+
+   }
+}
