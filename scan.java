1a046c657432e7c46e16b53a440183e34be5e899
Yun <gaogao0122@hotmail.com>
2017-07-12 15:14:42 -0400

    Add files via upload

--- scan.java
+++ scan.java
@@ -11,47 +11,44 @@ import java.io.RandomAccessFile;
 public class scan {
 	private boolean flag = false;
 	private String tableName;
-	private String tempString = null;
-	private String path = main.path;
+//	private String path = main.path;
 	private int totalLineNum;
-	private long chars=0;//chars指的是字符数 
+	BufferedReader br;
+	
 	public scan(String tableName) {
 		this.totalLineNum = getFileLine(tableName);
 		this.tableName = tableName;
-	}
-
-	public StringBuilder scanFile(){
-		StringBuilder sb = new StringBuilder();
-		String fileName = path+tableName+".csv";
-        File file = new File(fileName);  
-          
-        RandomAccessFile rf = null;   
-        
-        try {
-			rf = new RandomAccessFile(fileName, "r");
-	        rf.seek(chars);   
-	        
-	        tempString = rf.readLine();
-	        chars = rf.getFilePointer(); 
-	        sb.append(tempString);
-	        tempString = rf.readLine();
-	        if(tempString == null){
-	        	flag = true;
-				chars = 0;
-	        }
-	        
-	        
+ 		File file = new File(main.path + tableName+".csv");
+//		File file = new File(tableName+".csv");
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(file);
+			br = new BufferedReader(new InputStreamReader(fis));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+	}
+
+	public StringBuilder scanFile(){
+  		StringBuilder sb = new StringBuilder();
+ 		String line = "";
+  		try {
+			line = br.readLine();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}  
-          
-        
+		}
+		if (line != null) {
+			sb.append(line);
+		} else {
+			return null;
+		}
 		return sb;
 	}
+	
+	
 	
 	public int getFileLine(String tableName){
 		File file = new File(tableName+".csv");
@@ -78,17 +75,4 @@ public class scan {
 	public int getTotalLineNum() {
 		return totalLineNum;
 	}
-
-	public void setTotalLineNum(int totalLineNum) {
-		this.totalLineNum = totalLineNum;
-	}
-
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}	
-	
 }
